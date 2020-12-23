fca47bc50c747a76eca1e820e0eec5eaea4fa37d
harshit saxena <harshitsaxena586@gmail.com>
2020-12-23 22:53:05 +0530

    minor fixes

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
-var buttonOne = document.getElementById('clickme')
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+var btnTranslate =document.querySelector("translateBtn")
 
-buttonOne.addEventListener("click",function clickTracker()
-
-{
-    console.log('clicked')
-})
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
 
+function getTranslationURL(input) {
+    return serverURL + "?" + "text=" + input
+}
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something wrong with server! try again after some time")
+}
 
 
+function clickHandler() {
+    var inputText = txtInput.value;
 
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText; 
+           })
+        .catch(errorHandler)
+};
+
+btnTranslate.addEventListener("click", clickHandler) 
