f78a815cdd032157065f7788107ce57b3494879a
harshit saxena <harshitsaxena586@gmail.com>
2020-12-25 01:30:13 +0530

    some style changes

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 var button =   document.querySelector("#translateBtn")
 var input = document.querySelector("#txt-input")
 var output = document.querySelector("#output")
+
 url="https://api.funtranslations.com/translate/dothraki.json"
-// https://api.funtranslations.com/translate/dothraki.json
 function likgenerator(text){
     return url+"?"+ "text="+ text;
 
 }
-
+var temp =""
 function clickHandler(){
     var userInput=input.value
     fetch(likgenerator(userInput))
      .then(response=> response.json())
-     .then(json=>  output.innerHTML=json.contents.translated)
+     .then(json=>  temp=json.contents.translated)
+            output.innerHTML= temp
      
 }
 
 button.addEventListener("click", clickHandler)
+
